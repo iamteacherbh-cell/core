6bf35f5e5d71da54bcddcf73c14cb5492cc1e1af
iamteacherbh-cell <iamteacherbh@gmail.com>
2026-03-10 07:23:18 +0300

    Create page.tsx

---
+++ app/check-domain/page.tsx
@@ -0,0 +1,9 @@
+import { DomainChecker } from "@/components/sections/domain-checker";
+
+export default function CheckDomainPage() {
+  return (
+    <div className="min-h-screen bg-slate-900">
+      <DomainChecker />
+    </div>
+  );
+}
